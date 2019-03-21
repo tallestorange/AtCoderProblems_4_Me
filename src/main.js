9dd4d01292e3926c316896c11584e2af7e0bdd82
tallestorange <tallestorangeyuhei@gmail.com>
2019-03-22 02:10:57 +0900

    IndexedDBのテスト成功

--- src/main.js
+++ src/main.js
@@ -8,12 +8,11 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "font-awesome/css/font-awesome.css";
 import db from './idb';
 
+Vue.prototype.$db = db
 Vue.config.productionTip = false;
-console.log(db)
 
 new Vue({
   router,
   store,
-  db,
   render: h => h(App)
 }).$mount("#app");
